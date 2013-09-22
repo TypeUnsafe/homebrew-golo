aa383ef417876f840c425734c6ffd00ade27e6e0
Romain Lespinasse <romain.lespinasse@gmail.com>
2013-09-22 10:43:56 +0200

    golio formula

---
+++ golio.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Golio < Formula
+  head 'https://github.com/rlespinasse/golio.git', :branch => 'master'
+
+  def install
+    # setup absolute path for 'homebrew' golio
+    inreplace 'golio' do |s|
+      s.gsub! /^TOOLS_PATH.*/, "TOOLS_PATH=#{libexec}"
+    end
+
+    libexec.install Dir['*']
+    bin.install_symlink "#{libexec}/golio"
+  end
+end
