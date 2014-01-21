e2177988bfe07863e91fb34c4b1b8fd31f0216df
Romain Lespinasse <romain.lespinasse@gmail.com>
2014-01-21 23:19:17 +0100

    Third test for "m33ki" homebrew formula

--- m33ki.rb
+++ m33ki.rb
@@ -4,11 +4,6 @@ class M33ki < Formula
   head 'https://github.com/TypeUnsafe/m33ki.git', :branch => 'master'
 
   def install
-    # setup absolute path for 'homebrew' m33ki
-    inreplace 'm33ki' do |s|
-      s.gsub! /^TOOLS_PATH.*/, "TOOLS_PATH=#{libexec}"
-    end
-
     libexec.install Dir['*']
     bin.install_symlink "#{libexec}/m33ki"
   end
