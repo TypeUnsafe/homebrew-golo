7ae9fae223eb8e05b63e498d48fb564e169a9b5c
Romain Lespinasse <romain.lespinasse@gmail.com>
2014-03-11 21:33:15 +0100

    Golo move to 0-preview11

--- golo.rb
+++ golo.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Golo < Formula
   homepage 'http://golo-lang.org'
-  url 'http://search.maven.org/remotecontent?filepath=org/golo-lang/golo/0-preview10/golo-0-preview10-distribution.tar.gz'
-  sha1 '4b77201081afa4cd404a11644480f11945d41396'
-  version "0-preview10"
+  url 'http://search.maven.org/remotecontent?filepath=org/golo-lang/golo/0-preview11/golo-0-preview11-distribution.tar.gz'
+  sha1 '3a016ffb72b3d8696f22b5878c31763a8e05d9a4'
+  version "0-preview11"
   
   def install
     rm_f Dir["bin/*.bat"]
