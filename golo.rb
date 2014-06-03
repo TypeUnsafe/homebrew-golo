dddf16f65b064a09321193e65b4b37720243a33b
Romain Lespinasse <romain.lespinasse@gmail.com>
2014-06-03 13:01:59 +0200

    Golo move to v0-preview12

--- golo.rb
+++ golo.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Golo < Formula
   homepage 'http://golo-lang.org'
-  url 'http://search.maven.org/remotecontent?filepath=org/golo-lang/golo/0-preview11/golo-0-preview11-distribution.tar.gz'
-  sha1 '3a016ffb72b3d8696f22b5878c31763a8e05d9a4'
-  version "0-preview11"
+  url 'http://search.maven.org/remotecontent?filepath=org/golo-lang/golo/0-preview12/golo-0-preview12-distribution.tar.gz'
+  sha1 '6a46f3bdc5979e6ec0abaf6a6fd42a2d6e1e9bed'
+  version "0-preview12"
 
   head do
     # SNAPSHOT version
